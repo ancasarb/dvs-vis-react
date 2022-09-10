ff130fce30038bf94ddcc7af62c07ca64955c033
Anca Sarb <sarb.anca@gmail.com>
2022-09-10 18:19:21 +0100

    add working example of chart

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const props = { height: 480, width: 800 };
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App {...props} />
   </React.StrictMode>
 );
